2ceaab5d454b4a16b264d9d90f1242aac6ae5d71
Abohi <abohievanson@gmail.com>
2020-03-05 16:06:14 +0100

    part 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,105 +11,161 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase Storage',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String imageLink;
+  bool _isImagePicked = false;
+  File _image;
+  bool _isLoading = false;
+  bool _taskUploaded = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        body: Container(
+      width: double.infinity,
+      child: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 64,
+                ),
+                _isImagePicked
+                    ? Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 32.0),
+                        child: Container(
+                            height: 150.0,
+                            decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    width: 0.20,
+                                    color: Colors.grey[500],
+                                  ),
+                                ),
+                                image: DecorationImage(
+                                    image: FileImage(_image),
+                                    fit: BoxFit.cover))),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                        child: Container(
+                          height: 150.0,
+                          decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  width: 0.20,
+                                  color: Colors.grey[500],
+                                ),
+                              ),
+                              color: Colors.black),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Center(
+                                child: Text(
+                              "Please Select an Image",
+                              style: TextStyle(color: Colors.white),
+                            )),
+                          ),
+                        ),
+                      ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: RaisedButton(
+                        child: Text('Pick Image'),
+                        onPressed: () async {
+                          var imageTemp = await ImagePicker.pickImage(
+                              source: ImageSource.gallery);
+                          setState(() {
+                            _image = imageTemp;
+                            _isImagePicked = true;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 10.0),
+                    Expanded(
+                      flex: 1,
+                      child: Builder(
+                        builder: (context) => RaisedButton(
+                          child: Text('Upload Image'),
+                          onPressed: () async {
+                            if (_isImagePicked) {
+                              setState(() {
+                                _isLoading = true;
+                              });
+                              FirebaseStorage fs = FirebaseStorage.instance;
+                              StorageReference mRef =
+                                  fs.ref().child("pictures");
+                              mRef
+                                  .putFile(_image)
+                                  .onComplete
+                                  .then((storageTask) async {
+                                String link =
+                                    await storageTask.ref.getDownloadURL();
+
+                                setState(() {
+                                  _isLoading = false;
+                                  _taskUploaded = true;
+                                  imageLink = link;
+                                });
+                              });
+                            } else {
+                              var mSnackBar = SnackBar(
+                                content: Text(
+                                    "Please Pick an Image, before uploading"),
+                                duration: Duration(seconds: 3),
+                              );
+                              Scaffold.of(context).showSnackBar(mSnackBar);
+                            }
+                          },
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text("Image Uploaded to Strorage: ",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500)),
+                _taskUploaded
+                    ? Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 25.0),
+                        child: Container(
+                          width: double.infinity,
+                          height: 150.0,
+                          child: Image.network(imageLink),
+                        ),
+                      )
+                    : Container(
+                        height: 50.0,
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Center(child: Text("None"))))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
